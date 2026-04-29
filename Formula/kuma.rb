6d765d05733a0fc0e5ce5a99648dc4cfbaa3d2c7
Joan Subirats Llaveria <subirats345@gmail.com>
2026-04-29 20:46:18 +0900

    Update Kuma to 0.4.1

--- Formula/kuma.rb
+++ Formula/kuma.rb
@@ -3,8 +3,8 @@ require "base64"
 class Kuma < Formula
   desc "Tiny native Markdown-to-PDF renderer for macOS"
   homepage "https://github.com/subirats345/kuma"
-  url "https://github.com/subirats345/kuma/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "e6739be02e81a0c1384e28b48beff402b8e219f7a340e134406389232dac1687"
+  url "https://github.com/subirats345/kuma/archive/refs/tags/v0.4.1.tar.gz"
+  sha256 "499408391c7ee2b95e328d5747f74464faee0f8737999ad108b522d90b64ddc0"
   license "MIT"
   depends_on :macos
   depends_on macos: :sonoma
@@ -38,7 +38,7 @@ class Kuma < Formula
     BASE64
     (testpath/"sample.png").binwrite(Base64.decode64(sample_png))
 
-    assert_match "Kuma 0.4.0", shell_output("#{bin}/kuma --version")
+    assert_match "Kuma 0.4.1", shell_output("#{bin}/kuma --version")
     system bin/"kuma", "sample.md", "sample.pdf"
     assert_path_exists testpath/"sample.pdf"
     assert_predicate testpath/"sample.pdf", :size?

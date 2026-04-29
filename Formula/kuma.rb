87e0a07d3616e6c3ebc166e53d226dd41998046e
Joan Subirats Llaveria <subirats345@gmail.com>
2026-04-29 21:49:12 +0900

    Update Kuma to 0.6.0

--- Formula/kuma.rb
+++ Formula/kuma.rb
@@ -3,8 +3,8 @@ require "base64"
 class Kuma < Formula
   desc "Tiny native Markdown-to-PDF renderer for macOS"
   homepage "https://github.com/subirats345/kuma"
-  url "https://github.com/subirats345/kuma/archive/refs/tags/v0.5.0.tar.gz"
-  sha256 "dfcc65beee89fd0be9975ca519ac2c173a1e14f49c72c7a80e56a39763295462"
+  url "https://github.com/subirats345/kuma/archive/refs/tags/v0.6.0.tar.gz"
+  sha256 "c4c52e037f3a8b746d610e86d88e332763ec2f0731ffe56efcfba4728764447f"
   license "MIT"
   depends_on :macos
   depends_on macos: :sonoma
@@ -38,7 +38,7 @@ class Kuma < Formula
     BASE64
     (testpath/"sample.png").binwrite(Base64.decode64(sample_png))
 
-    assert_match "Kuma 0.5.0", shell_output("#{bin}/kuma --version")
+    assert_match "Kuma 0.6.0", shell_output("#{bin}/kuma --version")
     system bin/"kuma", "init", "starter.md"
     assert_path_exists testpath/"starter.md"
     system bin/"kuma", "starter.md", "starter.pdf"
@@ -48,5 +48,10 @@ class Kuma < Formula
     system bin/"kuma", "sample.md", "sample.pdf"
     assert_path_exists testpath/"sample.pdf"
     assert_predicate testpath/"sample.pdf", :size?
+
+    interactive_output = pipe_output("#{bin}/kuma", "sample.md\ninteractive.pdf\nn\nn\n", 0)
+    assert_match "Interactive PDF render", interactive_output
+    assert_path_exists testpath/"interactive.pdf"
+    assert_predicate testpath/"interactive.pdf", :size?
   end
 end
